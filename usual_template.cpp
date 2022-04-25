811829e28acb4a5deae685caeb2bb5483b8bdb70
mirakilov-developer <69168907+mirakilov-developer@users.noreply.github.com>
2022-04-25 14:25:05 +0500

    Create usual_template.cpp

---
+++ usual_template.cpp
@@ -0,0 +1,43 @@
+// MDSPro
+
+#include <bits/stdc++.h>
+
+#define trav(i,n) for(int i = 0; i < (n); i++)
+#define pb push_back
+#define se second
+#define fi first
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+const ld PI = 3.141592653589793;
+const ll MOD = 1e9+7;
+const int INF = 1e9;
+const ll INFLL = 4e18;
+const double EPS = 1e-9;
+const int SIZE = 1000*1007;
+
+void solve(int NT){
+    
+}
+
+
+#define TESTCASES
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    #ifdef TESTCASES
+        cin >> t;
+    #endif
+    
+    for(int i = 1; i <= t; ++i){
+        solve(i);
+        cout << "\n";
+    }
+}
