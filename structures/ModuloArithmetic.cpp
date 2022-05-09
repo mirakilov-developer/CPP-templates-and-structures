dbdc9db8618a9be9e744916be9a18274208717a2
mirakilov-developer <69168907+mirakilov-developer@users.noreply.github.com>
2022-05-09 20:57:56 +0500

    Create ModuloArithmetic.cpp

---
+++ structures/ModuloArithmetic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+const int MOD = 1e9+5;
+// Arithmetic by MODULO
+// ver 0.2.0
+struct Mint{
+    int x;
+    Mint(int a=0):x(a){}
+    
+    Mint operator+(const Mint &y)const{return Mint((1LL*x+y.x)%MOD);}
+    Mint operator+(int a)const{return Mint((1LL*x+a)%MOD);}
+    
+    Mint& operator=(const Mint &y){x = y.x; return *this;}
+    Mint& operator=(int a){x = a; return *this;}
+    
+    Mint& operator+=(const Mint &y){x = (1LL*x+y.x)%MOD; return *this;}
+    Mint& operator+=(int a){x = (1LL*x+a)%MOD; return *this;}
+};
+
+ostream& operator<<(ostream& os, const Mint& y){os << y.x;return os;}
+
+int main() {
+    Mint a = 300;
+    cout << a+5;
+    
+    return 0;
+}
