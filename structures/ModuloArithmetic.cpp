4d12c35255f7590b22a73a85e96750c86a6bfde4
mirakilov-developer <69168907+mirakilov-developer@users.noreply.github.com>
2022-05-13 12:48:09 +0500

    Update ModuloArithmetic.cpp

--- structures/ModuloArithmetic.cpp
+++ structures/ModuloArithmetic.cpp
@@ -4,21 +4,29 @@ using namespace std;
 
 const int MOD = 1e9+7;
 // Arithmetic by MODULO
-// ver 0.2.0
+// ver 0.3.0
 struct Mint{
     int x;
     Mint(int a=0):x(a){}
     
-    Mint operator+(const Mint &y)const{return Mint((1LL*x+y.x)%MOD);}
-    Mint operator+(int a)const{return Mint((1LL*x+a)%MOD);}
-    
     Mint& operator=(const Mint &y){x = y.x; return *this;}
     Mint& operator=(int a){x = a; return *this;}
     
+    Mint operator+(const Mint &y)const{return Mint((1LL*x+y.x)%MOD);}
+    Mint operator+(int a)const{return Mint((1LL*x+a)%MOD);}
     Mint& operator+=(const Mint &y){x = (1LL*x+y.x)%MOD; return *this;}
     Mint& operator+=(int a){x = (1LL*x+a)%MOD; return *this;}
+    
+    Mint operator-(const Mint &y)const{return Mint((1LL*x-y.x+MOD)%MOD);}
+    Mint operator-(int a)const{return Mint((1LL*x-a+MOD)%MOD);}
+    Mint& operator-=(const Mint &y){x = (1LL*x-y.x+MOD)%MOD; return *this;}
+    Mint& operator-=(int a){x = (1LL*x-a+MOD)%MOD; return *this;}
+    
+    Mint operator*(const Mint &y)const{return Mint((1LL*x*y.x)%MOD);}
+    Mint operator*(int a)const{return Mint((1LL*x*a)%MOD);}
+    Mint& operator*=(const Mint &y){x = (1LL*x*y.x)%MOD; return *this;}
+    Mint& operator*=(int a){x = (1LL*x*a)%MOD; return *this;}
 };
-
 ostream& operator<<(ostream& os, const Mint& y){os << y.x;return os;}
 
 int main() {
