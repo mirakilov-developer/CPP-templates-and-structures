277c7b34aa1079955d766d7ee22d12cb371c71d1
mirakilov-developer <69168907+mirakilov-developer@users.noreply.github.com>
2022-05-17 09:56:58 +0500

    Update ModuloArithmetic.cpp

--- structures/ModuloArithmetic.cpp
+++ structures/ModuloArithmetic.cpp
@@ -4,8 +4,25 @@ using namespace std;
 
 const int MOD = 1e9+7;
 // Arithmetic by MODULO
-// ver 0.3.0
+// ver 0.4.0
 struct Mint{
+    Mint binpow(int z, int b){
+        Mint ans = 1;
+        Mint a = z;
+        while(b){
+            if(b&1) ans *= a;
+            a *= a;
+            b >>= 1;
+        }
+        
+        return ans;
+    }
+    
+    int inverse(int z){
+        assert(__gcd(z,MOD) == 1);
+        return binpow(z,MOD-2).x;
+    }
+    
     int x;
     Mint(int a=0):x(a){}
     
@@ -26,6 +43,11 @@ struct Mint{
     Mint operator*(int a)const{return Mint((1LL*x*a)%MOD);}
     Mint& operator*=(const Mint &y){x = (1LL*x*y.x)%MOD; return *this;}
     Mint& operator*=(int a){x = (1LL*x*a)%MOD; return *this;}
+    
+    Mint operator/(const Mint &y){return Mint((1LL*x*inverse(y.x))%MOD);}
+    Mint operator/(int a){return Mint((1LL*x*inverse(a))%MOD);}
+    Mint& operator/=(const Mint &y){x = (1LL*x*inverse(y.x))%MOD; return *this;}
+    Mint& operator/=(int a){x = (1LL*x*inverse(a))%MOD; return *this;}
 };
 ostream& operator<<(ostream& os, const Mint& y){os << y.x;return os;}
 
